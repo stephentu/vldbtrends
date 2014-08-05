d8d9c37b82e26fdd65b514d870b59df3925d8878
Stephen Tu <stephentu@csail.mit.edu>
2014-08-05 15:13:20 -0700

    use a dirichlet-multinomial model

--- cluster.py
+++ cluster.py
@@ -8,6 +8,7 @@ import scipy.cluster
 import multiprocessing as mp
 import itertools as it
 import time
+import pickle
 
 from sklearn.cluster import KMeans
 #from util import Canonicalizer
@@ -21,7 +22,7 @@ from prettyplotlib import plt
 # This is "import matplotlib as mpl" from the prettyplotlib library
 from prettyplotlib import mpl
 
-from microscopes.models import gp as gamma_poisson
+from microscopes.models import gp as gamma_poisson, dm as dirichlet_multinomial
 from microscopes.mixture.definition import model_definition
 from microscopes.cxx.common.rng import rng
 from microscopes.cxx.common.recarray.dataview import numpy_dataview
@@ -57,10 +58,11 @@ def kmeans_algo(nclusters):
     return algo
 
 # multiprocessing boilerplate we will get rid of eventually
-def _make_definition(N, D):
+
+def _make_definition_gamma_poisson(N, D):
     return model_definition(N, [gamma_poisson]*D)
 
-def _make_hparams(D):
+def _make_hparams_gamma_poisson(D):
   hparams = {
     y : {
       'alpha': (log_exponential(1.), 1.),
@@ -70,16 +72,16 @@ def _make_hparams(D):
   }
   return hparams
 
-def _revive(N, D, data, latent):
-    defn = _make_definition(N, D)
+def _revive_gamma_poisson(N, D, data, latent):
+    defn = _make_definition_gamma_poisson(N, D)
     view = numpy_dataview(data)
     latent = deserialize(defn, latent)
-    hparams = _make_hparams(D)
+    hparams = _make_hparams_gamma_poisson(D)
     return defn, view, latent, hparams
 
-def _work(args):
+def _work_gamma_poisson(args):
     (N, D), data, latent, iters = args
-    defn, view, latent, hparams = _revive(N, D, data, latent)
+    defn, view, latent, hparams = _revive_gamma_poisson(N, D, data, latent)
     r = rng()
     model = bind(latent, view)
     for _ in xrange(iters):
@@ -87,12 +89,30 @@ def _work(args):
         #slice.hp(model, r, hparams=hparams)
     return latent.serialize()
 
-def mixturemodel_gamma_poisson(nchains=100, nitersperchain=1000):
+def _make_definition_dirichlet_multinomial(N, D):
+    return model_definition(N, [dirichlet_multinomial(D)])
+
+def _revive_dirichlet_multinomial(N, D, data, latent):
+    defn = _make_definition_dirichlet_multinomial(N, D)
+    view = numpy_dataview(data)
+    latent = deserialize(defn, latent)
+    return defn, view, latent
+
+def _work_dirichlet_multinomial(args):
+    (N, D), data, latent, iters = args
+    defn, view, latent = _revive_dirichlet_multinomial(N, D, data, latent)
+    r = rng()
+    model = bind(latent, view)
+    for _ in xrange(iters):
+        gibbs.assign(model, r)
+    return latent.serialize()
+
+def mixturemodel_gamma_poisson(nchains=100, nitersperchain=1000, pickle_file='results.p'):
     def algo(counts):
         data = smooth_ints(counts)
         N, D = data.shape
         latents = []
-        defn = _make_definition(N, D)
+        defn = _make_definition_gamma_poisson(N, D)
         Y = np.array([tuple(y) for y in data], dtype=[('',np.int32)]*D)
         view = numpy_dataview(Y)
         r = rng()
@@ -101,73 +121,109 @@ def mixturemodel_gamma_poisson(nchains=100, nitersperchain=1000):
             latents.append(latent.serialize())
         p = mp.Pool(processes=mp.cpu_count() * 2)
         start_time = time.time()
-        infers = p.map_async(_work, [((N, D), Y, latent, nitersperchain) for latent in latents]).get(10000000)
+        infers = p.map_async(_work_gamma_poisson, [((N, D), Y, latent, nitersperchain) for latent in latents]).get(10000000)
         print "inference took", (time.time() - start_time), "secs"
         p.close()
         p.join()
-        infers = [_revive(N, D, Y, infer)[2] for infer in infers]
+        with open(pickle_file, 'w') as fp:
+            pickle.dump(list(infers), fp)
+        infers = [_revive_gamma_poisson(N, D, Y, infer)[2] for infer in infers]
         assignments_samples = [s.assignments() for s in infers]
         return assignments_samples
     return algo
 
-def scipy_fcluster_merge_strategy(assignments):
+def mixturemodel_dirichlet_multinomial(nchains=100, nitersperchain=1000, pickle_file='results.p'):
+    def algo(counts):
+        data = smooth_ints(counts)
+        N, D = data.shape
+        latents = []
+        defn = _make_definition_dirichlet_multinomial(N, D)
+        Y = np.array([(list(y),) for y in data], dtype=[('',np.int32, (D,))])
+        view = numpy_dataview(Y)
+        r = rng()
+        for _ in xrange(nchains):
+            latent = initialize(defn, view, r=r)
+            latents.append(latent.serialize())
+        p = mp.Pool(processes=mp.cpu_count() * 2)
+        start_time = time.time()
+        infers = p.map_async(_work_dirichlet_multinomial, [((N, D), Y, latent, nitersperchain) for latent in latents]).get(10000000)
+        print "inference took", (time.time() - start_time), "secs"
+        p.close()
+        p.join()
+        with open(pickle_file, 'w') as fp:
+            pickle.dump(list(infers), fp)
+        infers = [_revive_dirichlet_multinomial(N, D, Y, infer)[2] for infer in infers]
+        assignments_samples = [s.assignments() for s in infers]
+        return assignments_samples
+    return algo
 
-    # Z-matrix helpers
-    def groups(assignments):
-        cluster_map = {}
-        for idx, gid in enumerate(assignments):
-            lst = cluster_map.get(gid, [])
-            lst.append(idx)
-            cluster_map[gid] = lst
-        return tuple(sorted(map(tuple, cluster_map.values()), key=len, reverse=True))
+def scipy_fcluster_merge_strategy(zmat_file_prefix='zmat'):
+    def merge(assignments):
+        # Z-matrix helpers
+        def groups(assignments):
+            cluster_map = {}
+            for idx, gid in enumerate(assignments):
+                lst = cluster_map.get(gid, [])
+                lst.append(idx)
+                cluster_map[gid] = lst
+            return tuple(sorted(map(tuple, cluster_map.values()), key=len, reverse=True))
 
-    def zmatrix(assignments_samples):
-        n = len(assignments_samples[0])
-        # should make sparse matrix
-        zmat = np.zeros((n, n), dtype=np.float32)
-        for assignments in assignments_samples:
-            clusters = groups(assignments)
-            for cluster in clusters:
-                for i, j in it.product(cluster, repeat=2):
-                    zmat[i, j] += 1
-        zmat /= float(len(assignments_samples))
-        return zmat
+        def zmatrix(assignments_samples):
+            n = len(assignments_samples[0])
+            # should make sparse matrix
+            zmat = np.zeros((n, n), dtype=np.float32)
+            for assignments in assignments_samples:
+                clusters = groups(assignments)
+                for cluster in clusters:
+                    for i, j in it.product(cluster, repeat=2):
+                        zmat[i, j] += 1
+            zmat /= float(len(assignments_samples))
+            return zmat
 
-    def reorder_zmat(zmat, order):
-        zmat = zmat[order]
-        zmat = zmat[:,order]
-        return zmat
+        def reorder_zmat(zmat, order):
+            zmat = zmat[order]
+            zmat = zmat[:,order]
+            return zmat
 
-    def linkage(zmat):
-        assert zmat.shape[0] == zmat.shape[1]
-        zmat0 = np.array(zmat[np.triu_indices(zmat.shape[0], k=1)])
-        zmat0 = 1. - zmat0
-        return scipy.cluster.hierarchy.linkage(zmat0)
+        def linkage(zmat):
+            assert zmat.shape[0] == zmat.shape[1]
+            zmat0 = np.array(zmat[np.triu_indices(zmat.shape[0], k=1)])
+            zmat0 = 1. - zmat0
+            return scipy.cluster.hierarchy.linkage(zmat0)
 
-    def ordering(l):
-        return np.array(scipy.cluster.hierarchy.leaves_list(l))
+        def ordering(l):
+            return np.array(scipy.cluster.hierarchy.leaves_list(l))
 
-    zmat = zmatrix(assignments)
-    li = linkage(zmat)
+        zmat = zmatrix(assignments)
+        li = linkage(zmat)
 
-    # diagnostic: draw z-matrix
-    indices = ordering(li)
-    plt.imshow(reorder_zmat(zmat, indices),
-           cmap=plt.cm.binary, interpolation='nearest')
-    plt.savefig("zmat-before.pdf")
-    plt.close()
+        # diagnostic: draw z-matrix
+        indices = ordering(li)
+        plt.imshow(reorder_zmat(zmat, indices),
+               cmap=plt.cm.binary, interpolation='nearest')
+        plt.savefig("{}-before.pdf".format(zmat_file_prefix))
+        plt.close()
 
-    fassignment = scipy.cluster.hierarchy.fcluster(li, 0.001)
-    clustering = groups(fassignment)
-    sorted_ordering = list(it.chain.from_iterable(clustering))
+        fassignment = scipy.cluster.hierarchy.fcluster(li, 0.001)
+        clustering = groups(fassignment)
+        sorted_ordering = list(it.chain.from_iterable(clustering))
 
-    # draw post fcluster() ordered z-matrix
-    plt.imshow(reorder_zmat(zmat, sorted_ordering),
-           cmap=plt.cm.binary, interpolation='nearest')
-    plt.savefig("zmat-after.pdf")
-    plt.close()
+        # draw post fcluster() ordered z-matrix
+        plt.imshow(reorder_zmat(zmat, sorted_ordering),
+               cmap=plt.cm.binary, interpolation='nearest')
+        plt.savefig("{}-after.pdf".format(zmat_file_prefix))
+        plt.close()
+        #return fassignment
+        return np.array(assignments[0])
 
-    return fassignment
+    return merge
+
+def unzip(zipped):
+    a, b = [], []
+    for x, y in zipped:
+        a.append(x)
+        b.append(y)
+    return a, b
 
 def cluster_and_render(conf,
                        dbname,
@@ -196,18 +252,15 @@ def cluster_and_render(conf,
     return words, np.array(ret)
 
   words, counts = vectors()
+  # exclude words which have culmulative count < filter
+  words, counts = unzip([(w, c) for w, c in zip(words, counts) if sum(c) >= 5])
+  counts = np.array(counts)
+
+  print 'len(words):', len(words)
 
   labels = merge_fn(algo_fn(counts))
   vects = smooth(counts)
   vects = np.array([[w] + list(v) for w, v in zip(words, vects)])
-  for v in vects[:10]:
-      print v
-
-  #clusterer = KMeans(nclusters, n_init=30, init='k-means++')
-  #data = vects[:,1:].astype(float)
-  #data = np.array([l / max(l) for l in data ])
-  #clusterer.fit(data) # words x year
-  #labels = clusterer.labels_
 
   xs = np.array(range(minyear, maxyear+1))
 
@@ -242,7 +295,6 @@ def cluster_and_render(conf,
       content.append(('', words, fname, idx))
 
 
-
   for label in set(labels):
     fig, ax = plt.subplots(1, figsize=(13, 5))
 
@@ -254,17 +306,6 @@ def cluster_and_render(conf,
     cluster = np.array(cluster)
     words = cluster[:,0]
     words = list(words)
-
-    #if 'crowd' in words:
-    #  data = cluster[:,1:].astype(float)
-    #  data = np.array([l / max(l) for l in data])
-    #  clusterer = KMeans(2)
-    #  clusterer.fit(data)
-    #  for newlabel in set(clusterer.labels_):
-    #    idxs = clusterer.labels_ == newlabel
-    #    subcluster = cluster[idxs]
-    #    add_content(subcluster, content, "%s-%s" % (label, newlabel))
-    #  continue
 
     cluster = cluster[:10]
     add_content(cluster, content, label)
@@ -282,7 +323,12 @@ if __name__ == '__main__':
 
   cluster_and_render('sigmod_kmeans', 'stats.db', outname="./text_kmeans.html")
 
-  cluster_and_render('sigmod_mixturemodel_gp', 'stats.db',
-          algo_fn=mixturemodel_gamma_poisson(),
-          merge_fn=scipy_fcluster_merge_strategy,
-          outname="./text_mixturemodel_gp.html")
+  #cluster_and_render('sigmod_mixturemodel_gp', 'stats.db',
+  #        algo_fn=mixturemodel_gamma_poisson(pickle_file='states_mixturemodel_gp.p'),
+  #        merge_fn=scipy_fcluster_merge_strategy(zmat_file_prefix='zmat_mixturemodel_gp'),
+  #        outname="./text_mixturemodel_gp.html")
+
+  cluster_and_render('sigmod_mixturemodel_dm', 'stats.db',
+          algo_fn=mixturemodel_dirichlet_multinomial(pickle_file='states_mixturemodel_dm.p'),
+          merge_fn=scipy_fcluster_merge_strategy(zmat_file_prefix='zmat_mixturemodel_dm'),
+          outname="./text_mixturemodel_dm.html")
